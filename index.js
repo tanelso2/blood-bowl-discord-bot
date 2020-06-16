ff9844227089fb66c7f01d9e4580e157487f4508
Thomas Nelson <tanelso2@gmail.com>
2020-06-15 21:53:42 -0700

    Basic skeleton

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const config = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+
+function advanceRound(message, user) {
+    message.channel.send(`no ${user}`);
+}
+
+function findOpponent(message, user) {
+    message.channel.send(`Fuck you ${user}`);
+}
+
+function printSchedule(message, user) {
+    message.channel.send(`I'm on my break, ${user}`);
+}
+
+const commands = {
+    'advance': advanceRound,
+    'opponent': findOpponent,
+    'schedule': printSchedule
+};
+
 client.once('ready', () => {
     console.log('Ready!');
 });
@@ -18,9 +37,16 @@ client.on('message', message => {
 
     if (message.mentions.has(client.user, mentionsOptions)) {
         // Should only trigger if they mention bot user by name
-        console.log(message.content);
-        console.log(message.cleanContent);
-        console.log(message.author.id);
+        //
+        const command = message.content.split(/ +/)[1];
+
+        const func = commands[command];
+
+        if(func) {
+            func(message, message.author);
+        } else {
+            message.channel.send(`Dude I have no idea what you're trying to say`);
+        }
     }
 
 
