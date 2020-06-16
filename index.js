db3799103e939a5e6913d223dc5e7af882ab670f
Thomas Nelson <tanelso2@gmail.com>
2020-06-15 21:28:12 -0700

    Only respond to direct mentions

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ const config = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-
 client.once('ready', () => {
     console.log('Ready!');
 });
 
 
 client.on('message', message => {
-    console.log(message.content);
+
+    const mentionsOptions = {
+        "ignoreDirect": false,
+        "ignoreRoles": true,
+        "ignoreEveryone": true
+    }
+
+
+    if (message.mentions.has(client.user, mentionsOptions)) {
+        // Should only trigger if they mention bot user by name
+        console.log(message.content);
+        console.log(message.cleanContent);
+        console.log(message.author.id);
+    }
+
+
 });
 
 client.login(config.token);
