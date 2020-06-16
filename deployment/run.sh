460e60ec905014527cd7c00595d7452538c8b855
Thomas Nelson <tanelso2@gmail.com>
2020-06-16 16:56:56 -0700

    Add some deployment files to the repo

---
+++ deployment/run.sh
@@ -0,0 +1,7 @@
+
+
+# Check for existence of nvm and install if not
+if false
+then
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+fi
---
+++ deployment/setup_box.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+
+BB_DIR=/opt/bb_bot
+
+git clone https://github.com/tanelso2/blood-bowl-discord-bot "${BB_DIR}"
+
+useradd -d "${BB_DIR}" bb_bot
+
+mkdir "${BB_DIR}/.ssh"
+
+cat "${BB_DIR}/.ssh/authorized_keys" <<EOF
+
+ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAG6VWZqT4z9ooOzOMWShpaZZD6WcI61qQR+aDd2palxMmpQ3F9qL6rzw0D2MTUd+7Obz6iBx2Cd3zxD4W1PX879pQB5TROtTrd0u5x/E5DaVRqwuErMb5PgNINqycvDmJXwXuAPvrd3H/poLDghbKMKF1V15dgEk/UwdONczapW9S6/gA== tnelson@Thomass-MacBook-Pro.local
+
+
+EOF
+
+cp -s "${BB_DIR}/deployment/discord-bot.service" "/etc/systemd/system/"
+
+systemctl daemon-reload
+
+systemctl enable discord-bot.service
+systemctl start discord-bot.service
+
+chown -R bb_bot:bb_bot "${BB_DIR}"
+
