ed3737a05ce32b0a8d598e81043179e5d59bc09f
Thomas Nelson <tanelso2@gmail.com>
2020-06-16 18:16:15 -0700

    Have games include references to the Coach objects

--- schedule.js
+++ schedule.js
@@ -1,35 +1,37 @@
 class Schedule {
-    constructor(data) {
+    constructor(data, coaches) {
+        function makeRound(data) {
+            return new Round(data, coaches);
+        }
         this.rounds = data.map(makeRound);
         this.rounds.sort((l, r) => { l.id - r.id; });
     }
 }
 
 class Round {
-    constructor(data) {
+    constructor(data, coaches) {
+        function makeGame(data) {
+            return new Game(data, coaches);
+        }
         this.id = data.round;
         this.games = data.games.map(makeGame);
     }
 }
 
 class Game {
-    constructor(data) {
+    constructor(data, coaches) {
         this.home = data.home;
         this.away = data.away;
+        this.homeCoach = coaches.find(c => c.teamName === this.home)
+        this.awayCoach = coaches.find(c => c.teamName === this.away)
     }
 
     get coaches() {
-        return [this.home_coach, this.away_coach];
+        return [this.homeCoach, this.awayCoach];
     }
 }
 
 // Mappable constructors
-function makeRound(data) {
-    return new Round(data);
-}
 
-function makeGame(data) {
-    return new Game(data);
-}
 
 module.exports = { Schedule };
