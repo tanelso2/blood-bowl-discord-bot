6a1e953655578ed498cca563d5bc794a7d27c353
Reese Robertson <rrobertson@purestorage.com>
2021-02-24 08:37:26 -0800

    Add Option unit tests

---
+++ src/core/types/option.spec.ts
@@ -0,0 +1,116 @@
+import { Option } from './option';
+
+describe('Option', () => {
+    describe('fromJsBullshit()', () => {
+        it('should transform null into None', () => {
+            Option.fromJsBullshit(null).on({
+                Some: (_) => {
+                    throw new Error("Should not generate Some(null)")
+                },
+                None: () => {},
+            });
+        });
+
+        it('should transform undefined into None', () => {
+            Option.fromJsBullshit(undefined).on({
+                Some: (_) => {
+                    throw new Error("Should not generate Some(null)")
+                },
+                None: () => {},
+            });
+        });
+
+        it('should transform NaN into None', () => {
+            Option.fromJsBullshit(NaN).on({
+                Some: (_) => {
+                    throw new Error("Should not generate Some(null)")
+                },
+                None: () => {},
+            });
+        });
+
+        it('should transform other falsy values into Some', () => {
+            Option.fromJsBullshit(false).on({
+                Some: (_) => {},
+                None: () => {
+                    throw new Error("Should not generate None")
+                }
+            });
+            Option.fromJsBullshit(0).on({
+                Some: (_) => {},
+                None: () => {
+                    throw new Error("Should not generate None")
+                }
+            });
+            Option.fromJsBullshit('').on({
+                Some: (_) => {},
+                None: () => {
+                    throw new Error("Should not generate None")
+                }
+            });
+        });
+    });
+
+    describe('unwrap()', () => {
+        it('should yield inner value from Some', () => {
+            const inner = 42;
+            const opt = Option.Some(inner);
+            try {
+                if (opt.unwrap() != inner) {
+                    throw new Error(`inner value ${opt.unwrap()} of Some did not match expected inner value of ${inner}`);
+                }
+            } catch (e) {
+                throw new Error('Should not throw in unwrap of Some');
+            }
+        });
+
+        it('should throw if called on None', (done) => {
+            try {
+                Option.None().unwrap();
+            } catch {
+                done();
+            } finally {
+                throw new Error("Should have thrown in unwrap of None");
+            }
+        });
+    });
+
+    describe('isSome()', () => {
+        it('should align with pattern matching', () => {
+            const opt = Option.Some(42);
+            if (!opt.isSome()) {
+                throw new Error("Some should declare isSome()");
+            } else if (opt.isNone()) {
+                throw new Error("Some should not declare isNone()");
+            }
+        });
+    });
+
+    describe('isNone()', () => {
+        it('should align with pattern matching', () => {
+            const opt = Option.None();
+            if (!opt.isNone()) {
+                throw new Error("None should declare isNone()");
+            } else if (opt.isSome()) {
+                throw new Error("None should not declare isSome()");
+            }
+        });
+    });
+
+    describe('pattern matching', () => {
+        it('should implement pattern matching', () => {
+            Option.Some(42).on({
+                Some: (_) => {},
+                None: () => {
+                    throw new Error("Some was matched to None");
+                },
+            });
+            Option.None().on({
+                Some: (_) => {
+                    throw new Error("None was matched to Some");
+                },
+                None: () => {},
+            });
+        });
+    });
+});
