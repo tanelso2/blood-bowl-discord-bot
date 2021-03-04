faa4b590768f5f0eacc4a42792f709ec5c881eb0
Thomas Nelson <tanelso2@gmail.com>
2021-03-03 23:55:01 -0800

    Re-add previous linting rules

--- src/core/types/option.ts
+++ src/core/types/option.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { PatternMatchable } from './pattern';
 
 export class Option<a> extends PatternMatchable {
