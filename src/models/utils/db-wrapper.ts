aecbe6a813b30828e94f121df7060fd4afd935df
Thomas Nelson <tanelso2@gmail.com>
2022-06-12 16:19:34 -0500

    Some cleanup and refactoring

--- src/models/utils/db-wrapper.ts
+++ src/models/utils/db-wrapper.ts
@@ -2,13 +2,8 @@ import { Option } from '@core/types/option';
 import * as fs from 'fs';
 import { Database } from 'sqlite3';
 import { DBResult } from './db-result';
+import {allAre, TypeGuard} from '@core/types/type-guards';
 
-
-function allVerify<a>(l: unknown[], verify: (x: unknown) => x is a): l is a[] {
-  return l.every(x => verify(x));
-}
-
-type verifyFn<a> = (x: unknown) => x is a
 
 export class DBWrapper extends Database {
   constructor(filename?: string) {
@@ -33,15 +28,15 @@ export class DBWrapper extends Database {
     return rows[0] as Partial<a>;
   }
 
-  async fetchAndVerify<a>(sql: string, parameters: any[], verify: (x: unknown) => x is a): Promise<a[]> {
+  async fetchAndVerify<a>(sql: string, parameters: any[], verify: TypeGuard<a>): Promise<a[]> {
     const results = await this.fetch<a>(sql, parameters);
-    if(!allVerify(results, verify)) {
+    if(!allAre(results, verify)) {
       throw new Error("fuck");
     }
     return results;
   }
 
-  async fetchAndVerifyOne<a>(sql: string, parameters: any[], verify: (x: unknown) => x is a): Promise<a> {
+  async fetchAndVerifyOne<a>(sql: string, parameters: any[], verify: TypeGuard<a>): Promise<a> {
     const r = await this.fetchOne<a>(sql, parameters);
     if(!verify(r)) {
       throw new Error("fuck");
@@ -49,14 +44,14 @@ export class DBWrapper extends Database {
     return r;
   }
 
-  async fetchResults<a>(sql: string, parameters: any[], verify: verifyFn<a>): Promise<DBResult<a[]>> {
+  async fetchResults<a>(sql: string, parameters: any[], verify: TypeGuard<a>): Promise<DBResult<a[]>> {
     let r;
     try {
       r = await this.fetch<a>(sql, parameters);
     } catch (e: any) {
       return DBResult.DBErr(e);
     }
-    if(!allVerify(r, verify)) {
+    if(!allAre(r, verify)) {
       return DBResult.Unverified(r);
     }
     return DBResult.Result(r);
