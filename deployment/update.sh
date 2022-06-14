fd2f7e6387236f0beec8e44053b62537c07cabaf
Thomas Nelson <tanelso2@gmail.com>
2022-06-14 03:27:10 -0500

    fixup! Oops forgot to git add the new file

--- deployment/update.sh
+++ deployment/update.sh
@@ -4,9 +4,10 @@ cd /opt/bb_bot/
 
 OUTPUT=$(git pull)
 
-if [[ $OUTPUT == "Already up to date" ]]; then
+if [[ $OUTPUT == "Already up to date." ]]; then
     echo "We done here"
 else
+    chown -R bb_bot:bb_bot /opt/bb_bot
     systemctl daemon-reload
     systemctl restart discord-bot.service
 fi
