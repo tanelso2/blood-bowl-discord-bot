0e0237e5be344daea469104b24d76d556159420a
Thomas Nelson <tanelso2@gmail.com>
2022-06-16 01:27:33 -0500

    Run checks in parallel using ts-node
    This was so hard to do. The bash version took a fraction of the time.
    Node.js and promises are awful about just swallowing up control flow and
    you never knowing why things aren't triggered.

--- scripts/make-league.ts
+++ scripts/make-league.ts
@@ -3,14 +3,11 @@ import 'module-alias/register';
 
 import * as process from 'process';
 import * as rd from 'readline';
-import * as path from 'path';
-import * as os from 'os';
-import { promises as fs } from 'fs';
-import { execSync } from 'child_process';
 
 import { CoachData } from '@models/coach';
 import { getLeagueFromFile, League, LeagueData, TournamentSeason } from '@models/league';
 import { RoundData } from '@models/round';
+import { editObjectInEditor } from '@utils/procUtils';
 
 
 const readline = rd.createInterface({
@@ -55,48 +52,6 @@ async function pickOne(choices: string[]): Promise<string> {
         return await pickOne(choices);
     }
 
-}
-
-/**
- * Interrupts current script,
- * opens file in $EDITOR,
- * waits for user to exit $EDITOR,
- * then resumes current script
- */
-function openFileInEditor(filename: string) {
-    let editor = process.env['EDITOR'];
-    if (!editor) {
-        throw new Error(`User has no EDITOR env variable set`);
-    }
-    return execSync(`${editor} ${filename}`, {
-        stdio: 'inherit',
-        encoding: 'utf-8'
-    });
-}
-
-/**
- * @param obj: 'a
- * @param toText: 'a -> string
- * @param fromText: string -> 'b
- * @returns: 'b
- */
-async function editObjectInEditor<a,b>(
-    obj: a,
-    toText: (x: a) => string,
-    fromText: (x: string) => b
-    ): Promise<b> {
-    const text = toText(obj);
-    const randomName = `tmp-${Math.floor(Math.random() * Math.floor(1000))}`;
-    const tmpFile = path.join(os.tmpdir(), randomName);
-    // create file
-    // write text to file
-    await fs.writeFile(tmpFile, text);
-    openFileInEditor(tmpFile);
-    // read file
-    const result = await fs.readFile(tmpFile, {encoding: 'utf-8'});
-    // translate file using `fromText`
-    // return
-    return fromText(result);
 }
 
 async function queryCoach(): Promise<CoachData> {
