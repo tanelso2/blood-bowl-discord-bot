8dc10c577d4b3b5dd6ae954b05a71060387b8134
Thomas Nelson <tanelso2@gmail.com>
2022-07-23 14:18:21 -0500

    Update setup_box.sh
    * remove haskell components
    * remove unused deploy key
    * use ssh instead of https for git clone

--- deployment/setup_box.sh
+++ deployment/setup_box.sh
@@ -7,24 +7,13 @@ curl -sL https://deb.nodesource.com/setup_12.x | bash -
 apt-get install -y nodejs
 
 
-# Install Haskell stack
-apt-get install -y haskell-stack
-
-stack upgrade --force-download
-
 BB_DIR=/opt/bb_bot
 
-git clone https://github.com/tanelso2/blood-bowl-discord-bot "${BB_DIR}"
+git clone git@github.com:tanelso2/blood-bowl-discord-bot.git "${BB_DIR}"
 
 useradd -d "${BB_DIR}" bb_bot
 
 mkdir "${BB_DIR}/.ssh"
-
-cat <<EOF > "${BB_DIR}/.ssh/authorized_keys"
-
-ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAG6VWZqT4z9ooOzOMWShpaZZD6WcI61qQR+aDd2palxMmpQ3F9qL6rzw0D2MTUd+7Obz6iBx2Cd3zxD4W1PX879pQB5TROtTrd0u5x/E5DaVRqwuErMb5PgNINqycvDmJXwXuAPvrd3H/poLDghbKMKF1V15dgEk/UwdONczapW9S6/gA== tnelson@Thomass-MacBook-Pro.local
-
-EOF
 
 chown -R bb_bot:bb_bot "${BB_DIR}"
 
