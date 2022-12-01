8322ec5a0fb72459c5dd17f0d74d3d6ee684058b
Thomas Nelson <tanelso2@gmail.com>
2022-11-30 22:55:05 -0600

    Created a template for other discord bots

--- index.ts
+++ index.ts
@@ -1,13 +1,8 @@
 import 'module-alias/register';
 
-import * as process from 'process';
 import { readFileSync } from 'fs';
 import { Client, Events, GatewayIntentBits, Message } from 'discord.js';
 import { logger } from '@core/logger';
-import { getLeagueFromFile, League} from '@models/league';
-import { ContextError, makeContext } from '@commands/index';
-import { CommandContext } from '@commands/core';
-import { generateInsult } from '@generator/helpers';
 
 const configFile = './config.json';
 
@@ -18,20 +13,6 @@ interface DiscordAuthConfig {
 const config = JSON.parse(readFileSync(configFile, 'utf-8')) as DiscordAuthConfig;
 
 const LOGGER = logger.child({module:'index'});
-
-const leagueFiles = process.argv.slice(2);
-if (leagueFiles.length === 0) {
-    console.error('USAGE: node index.js <league_file>...');
-    process.exit(1);
-}
-
-/**
- *
- * @returns {League[]}
- */
-function getAllLeagues(): League[] {
-    return leagueFiles.map(getLeagueFromFile);
-}
 
 
 const intents = [
@@ -49,9 +30,9 @@ client.once(Events.ClientReady, (_) => {
 
 async function handleMessage(message: Message, client: Client) {
     const mentionsOptions = {
-        "ignoreDirect": false,
-        "ignoreRoles": true,
-        "ignoreEveryone": true
+        ignoreDirect: false,
+        ignoreRoles: true,
+        ignoreEveryone: true
     };
 
 
@@ -59,33 +40,8 @@ async function handleMessage(message: Message, client: Client) {
     if (message.mentions.has(client.user!, mentionsOptions)) {
         // Should only trigger if they mention bot user by name
         //
-        await makeContext(client, getAllLeagues(), message).match<Promise<any>>({
-            Left: (e: ContextError) => {
-                switch (e.kind) {
-                    case "unknown-command": {
-                        const insult = generateInsult();
-                        return message.reply(`Dude I have no idea what you're trying to say\n${insult}`);
-                    }
-                    case "user-in-no-leagues": {
-                        return message.reply(`You don't seem to be in any leagues...`);
-                    }
-                    case "user-in-many-leagues": {
-                        const leagueNames: string = e.payload! as string;
-                        return message.reply(`Yeah you're going to need to be more specific. You're in these leagues: ${leagueNames}`);
-                    }
-                }
+        LOGGER.info(`Someone talked to the bot`);
 
-            },
-            Right: async (context: CommandContext) => {
-                try {
-                    await context.cmd.func(context);
-                } catch (e: any) {
-                    LOGGER.info(e);
-                    return message.channel.send(`Ooff I had a bit of a glitch there`);
-
-                }
-            }
-        });
     }
 }
 
--- src/commands/insult.ts
+++
@@ -1,7 +0,0 @@
-import { generateInsult } from '@generator/helpers';
-import { CommandContext } from './core';
-
-export async function printInsult({message}: CommandContext) {
-    const insult = generateInsult();
-    await message.reply(insult);
-}
